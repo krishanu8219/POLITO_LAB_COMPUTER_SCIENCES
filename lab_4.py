bc06dd9a5d53867ec69901ff0c363f02f7acc3f9
krishanu8219 <krishanukumar2002@gmail.com>
2024-01-24 01:42:17 +0100

    Completed evaluating exercise of  lab_4

--- lab_4.py
+++ lab_4.py
@@ -1,26 +1,31 @@
-x = []
-x_0 = int(input("Enter the value for x1: "))
-x_1 = int(input("Enter the value for x2: "))
+def main():
+    first_element = int(input("Enter the value for x1: "))
+    second_element = int(input("Enter the value for x2: "))
 
-b = [x_0,x_1]
+    b = [first_element,second_element]
+     
+    n = int(input("How many elements you want in the sequence? "))
+#printing the 'n' elements of the Fibonacci Series
 
-n = int(input("How many elements you want in the sequence? "))
+    x = []
+    for i in range(n):
+        if len(x) < 2:    
+            x.append(b[i])
+            print(x[i])
+        else:
+            x.append(x[i-1] + x[i -2])
+            print(x[i])
+# Count the number of elements in the sequence that are <= z
+    z = int(input("Enter the biggest number of your Series. "))
 
-for i in range(n):
-    if len(x) < 2:    
-        x.append(b[i])
-        print(x[i])
-    else:
-        x.append(x[i-1] + x[i -2])
-        print(x[i])
+    count = 0
+    for i in x:
+        if i <= z:
+            count += 1
+        else:
+            break 
 
-z = int(input("Enter the biggest number of your Series. "))
+    print(f"The number of elements in the sequence that are lower than or equal to the biggest number are {count}.")    
 
-count = 0
-for i in x:
-    if i <= z:
-        count += 1
-    else:
-        break 
-
-print(f"The number of elements that are lower than or equal to the biggest number are {count}")    
+if __name__ == "__main__":
+    main()
