bb3aa9b5776e250a215f818802027552ed30bf80
krishanu8219 <krishanukumar2002@gmail.com>
2024-01-24 02:16:52 +0100

    Completed evaluating exercise of  lab_3

--- lab_3.py
+++ lab_3.py
@@ -1,20 +1,41 @@
 def main():  
     real_value = float(input("Enter a real value"))
+
+    # Absolute value
     if real_value < 0:
-        absolute_value = -int(real_value)
-        floor_value = int(real_value) -1
-        ceiling_value = int(real_value)
-    elif real_value >= 0:
-        absolute_value = int(real_value)
+        absolute_value = -real_value
+    else:
+        absolute_value = real_value
+
+    # Floor value
+    if real_value >= 0:
         floor_value = int(real_value)
-        if real_value != 0:
-            ceiling_value = int(real_value) + 1
+    else:
+        if real_value == int(real_value):
+            floor_value = int(real_value)
         else:
-             ceiling_value = 0     
-    if real_value - int(real_value) >= 0.5:
-        round_value = int(real_value) + 1
-    elif real_value - int(real_value) <= 0.5:
-        round_value = int(real_value)     
+            floor_value = int(real_value) - 1
+
+    # Ceiling value
+    if real_value <= 0:
+        ceiling_value = int(real_value)
+    else:
+        if real_value == int(real_value):
+            ceiling_value = int(real_value)
+        else:
+            ceiling_value = int(real_value) + 1
+
+    # Round value
+    if real_value >= 0:
+        if real_value - int(real_value) >= 0.5:
+            round_value = int(real_value) + 1
+        else:
+            round_value = int(real_value)
+    else:
+        if (real_value - int(real_value)) <= -0.5:
+            round_value = int(real_value) - 1
+        else:
+            round_value = int(real_value)        
 
     print(f"Absolute value is {absolute_value} \nFloor Value is {floor_value} \nCeiling Value is {ceiling_value} \nRound Value is {round_value} ")
 
